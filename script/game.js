fe80aa688b1f94c6188626a7f70d6709d8bc3c8f
abusaiyedjoy <abusaiyedjoy1@gmail.com>
2024-02-24 22:00:14 +0600

    get random alphabet & set bg-color

--- script/game.js
+++ script/game.js
@@ -1,3 +1,6 @@
+
+// Playground and homescreen handle
+
 const playBtn = document.getElementById("play-btn");
 const playGround = document.getElementById("playGround");
 const homeScreen = document.getElementById("homeScreen");
@@ -5,29 +8,75 @@ const homeScreen = document.getElementById("homeScreen");
 document.addEventListener("keypress", (e) => {
   if (e.key === "Enter") {
     homeScreen.classList.add("hidden");
-    playGround.classList.remove("hidden");
+      playGround.classList.remove("hidden");
+      
+      keyboardPressed(event)
   }
 });
 
-playBtn.addEventListener("click", () => {
+playBtn.addEventListener("click", (event) => {
     homeScreen.classList.add("hidden");
     playGround.classList.remove("hidden");
+
+    keyboardPressed(event)
 })
 
 
 
 
-const currentRandomAlphabet = document.getElementById("random-alphabet");
+// Random alphabet generate and bg color
 
-function randomAlphabet() {
+function getRandomAlphabets() {
     const alphabetString = "abcdefghijklmnopqrstuvwxyx";
     const alphabets = alphabetString.split("");
 
-  const random = Math.random() * 25;
-  const index = Math.round(random);
+    const randomNumber = Math.round(Math.random() * 25);
 
-    const alphabet = alphabets[index];
-    
-    currentRandomAlphabet.innerText = alphabet;
+    const alphabet = alphabets[randomNumber];
+
+
+    return alphabet;
 }
 
+
+
+
+// Keyboard button pressed
+
+
+function keyboardPressed(event) {
+
+    const alphabet =    getRandomAlphabets(); 
+
+    const youPress = event.key;
+    console.log("you Press", youPress)
+    
+    const RandomAlphabet = document.getElementById("random-alphabet");
+    const currentAlphabet = (RandomAlphabet.textContent).toLowerCase();
+    console.log("currentAlphabet", currentAlphabet)
+    
+    const element = document.getElementById(alphabet);
+    element.classList.add("bg-orange-400");
+    
+    if (currentAlphabet == youPress) {
+      console.log("true");
+      const elementRemove = document.getElementById(alphabet);
+      elementRemove.classList.remove("bg-orange-400");
+    } else {
+        console.log("Sorry!!!");
+    }
+        
+
+    
+    const currentRandomAlphabet = document.getElementById("random-alphabet");
+    currentRandomAlphabet.textContent = alphabet;
+           
+        
+    }
+    
+    document.addEventListener("keypress", keyboardPressed);
+    
+    
+    
+    
+    
